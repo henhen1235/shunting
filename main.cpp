db2a2035cf6619d6506dc82d4c88b9a04147f2af
Henry Xu <henry.xu.aa.nl@gmail.com>
2025-02-13 21:54:27 -0800

    postifx

--- main.cpp
+++ main.cpp
@@ -12,59 +12,87 @@ void dequeue(Node*& questart);
 void destroy(Node *& stastart);
 
 int main(){
-  Node* questart = nullptr;
-  Node* stastart = nullptr;
-  while(true){
-  int num;
-  cin >> num;
-  if(num == 1){
-    char tempchar;
-    cin >> tempchar;
-    push(tempchar, stastart);
-  }
-  if(num == 2){
-    cout << peak(stastart) << endl;
-  }
-  if(num == 3){
-    pop(stastart);
-  }
-  if(num == 4){
-    while (stastart != NULL){
-      Node* temp = stastart;
-      stastart = stastart->getNext();
-      delete temp;
+  Node* endque = nullptr;
+  Node* midstack = nullptr;
+  char in[50];
+  cout << endl << "What would you like to input?(It cannot be longer then 50 and no spaces): ";
+  cin >> in;
+  int x = 0;
+  while(in[x] != '\0'){
+    char tempchar = in[x];
+    if(tempchar == '0' || tempchar == '1' || tempchar == '2' || tempchar == '3' || tempchar == '4' || tempchar == '5' || tempchar == '6' || tempchar == '7' || tempchar == '8' || tempchar == '9'){
+      enqueue(tempchar, endque);
+      x++;
     }
-    break;
+
+    else if(tempchar == '+' || tempchar == '-'){
+      while(peak(midstack) == '+' || peak(midstack) == '-' || peak(midstack) == '/' || peak(midstack) == '*' || peak(midstack) == '^'){
+        enqueue(peak(midstack),endque);
+        pop(midstack);
+      }
+      push(tempchar, midstack);
+      x++;
+    }
+
+    else if(tempchar == '*' || tempchar == '/'){
+      while(peak(midstack) == '/' || peak(midstack) == '*' || peak(midstack) == '^'){
+        enqueue(peak(midstack),endque);
+        pop(midstack);
+      }
+      push(tempchar, midstack);
+      x++;
+    }
+
+    else if(tempchar == '^'){
+      while(peak(midstack) == '^'){
+        enqueue(peak(midstack),endque);
+        pop(midstack);
+      }
+      push(tempchar, midstack);
+      x++;
+    }
+
+    else if(tempchar == '('){
+      push(tempchar, midstack);
+      x++;
+    }
+    else if(tempchar == ')'){
+      while(peak(midstack) != '(' ){
+        enqueue(peak(midstack),endque);
+        pop(midstack);
+      }
+      pop(midstack);
+      x++;
+    }
+    else{
+      cout << "error" << endl;
+      break;
+    }
   }
-  if(num == 5){
-    Node* tempstastart = stastart;
+  
+  while(midstack != nullptr) {
+    enqueue(peak(midstack), endque);
+    pop(midstack);
+  }
+
+  Node* tempstastart = endque;
     while(tempstastart != NULL){
       cout << tempstastart->getchar();
       tempstastart = tempstastart->getNext();
     }
-  }
-}
+
+    destroy(endque);
+destroy(midstack);
+
   return 0;
 }
 
 void push(char nchar, Node*& stastart){
-  /*
-  if(stastart == nullptr){
-    Node* newnode = new Node(nchar);
-    stastart = newnode;
-    return;
-  }
-  Node* tempnode = stastart; 
-  while(tempnode->getNext() != NULL){
-    tempnode = tempnode->getNext();
-  }
-  Node* newnode = new Node(nchar);
-  tempnode->setNext(newnode);
-*/
 Node* newnode = new Node(nchar);
 newnode->setNext(stastart);
 stastart = newnode;
-  }
+}
+
 
 char peak(Node* stastart){
   if(stastart == nullptr){
@@ -73,6 +101,7 @@ char peak(Node* stastart){
   return stastart->getchar();
 }
 
+
 void pop(Node*& stastart){
   if(stastart == nullptr){
     return;
@@ -80,18 +109,36 @@ void pop(Node*& stastart){
   Node* tempnode = stastart;
   stastart = stastart->getNext();
   delete tempnode;
-  /*
-  if(stastart->getNext() == nullptr){
-    delete stastart;
-    stastart = nullptr;
+}
+
+void destroy(Node *& stastart){
+  while (stastart != NULL){
+    Node* tempnode = stastart;
+    stastart = stastart->getNext();
+    delete tempnode;
+}
+}
+
+
+void enqueue(char nchar, Node*& questart){
+  if(questart == nullptr){
+    Node* newnode = new Node(nchar);
+    questart = newnode;
     return;
   }
-  Node* tempnode = stastart;
-  while(tempnode->getNext()->getNext() != NULL){
+  Node* tempnode = questart; 
+  while(tempnode->getNext() != NULL){
     tempnode = tempnode->getNext();
   }
-  Node* tempnode2 = tempnode->getNext();
-  delete tempnode2;
-  tempnode->setNext(NULL);
-*/
+  Node* newnode = new Node(nchar);
+  tempnode->setNext(newnode);
+}
+
+void dequeue(Node*& questart){
+  if(questart == nullptr){
+    return;
   }
+  Node* tempnode = questart;
+  questart = questart->getNext();
+  delete tempnode;
+}
