a88356c3a5ac6e3b5cfe8e726d29de9597b25397
Henry Xu <henry.xu.aa.nl@gmail.com>
2025-02-13 19:47:37 -0800

    minor fixes

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ char peak(Node* stastart);
 void pop(Node*& stastart);
 void enqueue(char nchar, Node*& questart);
 void dequeue(Node*& questart);
+void destroy(Node *& stastart);
 
 int main(){
   Node* questart = nullptr;
@@ -28,23 +29,26 @@ int main(){
     pop(stastart);
   }
   if(num == 4){
-    delete questart;
-    delete stastart;
+    while (stastart != NULL){
+      Node* temp = stastart;
+      stastart = stastart->getNext();
+      delete temp;
+    }
     break;
   }
   if(num == 5){
     Node* tempstastart = stastart;
-    while(tempstastart->getNext() != NULL){
+    while(tempstastart != NULL){
       cout << tempstastart->getchar();
       tempstastart = tempstastart->getNext();
     }
-    tempstastart = tempstastart->getNext();
   }
 }
   return 0;
 }
 
 void push(char nchar, Node*& stastart){
+  /*
   if(stastart == nullptr){
     Node* newnode = new Node(nchar);
     stastart = newnode;
@@ -56,15 +60,15 @@ void push(char nchar, Node*& stastart){
   }
   Node* newnode = new Node(nchar);
   tempnode->setNext(newnode);
-}
+*/
+Node* newnode = new Node(nchar);
+newnode->setNext(stastart);
+stastart = newnode;
+  }
 
 char peak(Node* stastart){
   if(stastart == nullptr){
-    char oops = '\0';
-    return oops;
-  }
-  while(stastart->getNext() != NULL){
-   stastart = stastart->getNext();
+    return '\0' ;
   }
   return stastart->getchar();
 }
@@ -74,10 +78,20 @@ void pop(Node*& stastart){
     return;
   }
   Node* tempnode = stastart;
+  stastart = stastart->getNext();
+  delete tempnode;
+  /*
+  if(stastart->getNext() == nullptr){
+    delete stastart;
+    stastart = nullptr;
+    return;
+  }
+  Node* tempnode = stastart;
   while(tempnode->getNext()->getNext() != NULL){
     tempnode = tempnode->getNext();
   }
   Node* tempnode2 = tempnode->getNext();
   delete tempnode2;
   tempnode->setNext(NULL);
-}
+*/
+  }
